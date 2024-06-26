8ece9b37ccf1515397c28f99cb9ccf70c92d60b1
unknown <nduflot@dawan.fr>
2024-06-26 10:46:37 +0200

    jquery CSS pt 2 .animate()

--- js/jqcss.js
+++ js/jqcss.js
@@ -19,5 +19,60 @@ jQuery(function($){
     });
 
     /* quand on clique sur #resSetPropValue, on remet la couleur d'origine */
+    $('#resSetPropValue').on('click', function(){
+        $('#resSetPropValue').css('color', 'blue');
+    });
+
+    /* objet litéral en JS */
+
+    const monObjLiteral = {
+        nom : 'Duflot',
+        prenom : 'Nicolas',
+        bonjour : function(){
+            return this.prenom;
+        }
+    };
+
+    console.log(monObjLiteral);
+    console.log(monObjLiteral.nom);
+    console.log(monObjLiteral.bonjour());
+
+    $('#animation01').on('click', function(){
+
+        /* animer blocAnimate01 */
+        $('#blocAnimate01').animate(
+            /* prop css a changer */
+            {
+                width: '250px',
+                fontSize : ['2rem', 'linear']
+            },
+            /* vitesse de l'animation */
+            1000,
+            'swing',
+            function(){
+                console.log('blocAnimate01 a grandi');
+            }
+        );
+        
+        /* animer blocAnimate02 */
+        $('#blocAnimate02').animate(
+            /* prop css a changer */
+            {
+                width: '250px',
+                fontSize : ['2rem', 'swing']
+            },
+            /* vitesse de l'animation */
+            1000,
+            'linear',
+            function(){
+                console.log('blocAnimate02 a grandi');
+            }
+        );
+
+    });
+
+    /* quand on clique sur #anim01Reset, on remet les blocs à leur apparence d'origine */
+
+    
 
 });
