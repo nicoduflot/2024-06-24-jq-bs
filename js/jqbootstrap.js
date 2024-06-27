7d4174e72f6e2e3a62ac799799de9635cfcef1fb
unknown <nduflot@dawan.fr>
2024-06-27 11:48:57 +0200

    jquery bootstrap pt3 : navs & tabs dynamiques factorisation d'un widget réutilisable

--- js/jqbootstrap.js
+++ js/jqbootstrap.js
@@ -6,10 +6,15 @@ jQuery(function ($) {
         $('html, body').scrollTop( $(document).height() );
     });
 
-
-    $('#favoriteTeenAgeAlbum a').on('click', function(event){
+    $('.favoriteContent li a').on('click', function(event){
+        /* on reprère le clique sur n'importe quel lien situé dans un élément ayant la classe favoriteContent */
+        /* on annule le comportement par défaut d'un clic sur un lien */
         event.preventDefault();
-        $('#favoriteTeenAgeAlbum a').each(function(){
+        /* on reprère l'id de l'élément ancètre le plus proche possèdant la classe favoriteContent */
+        const parent = $(this).closest('.favoriteContent').attr('id');
+        /*console.log(parent);*/
+        /* on exploite l'id dans le script pour faitre les remplacement de classe et d'attributs */
+        $(`#${parent} a`).each(function(){
             /* le $(this) dans ce each() correspond à chaque lien correspondant au sélecteur CSS (les lien contenu dans la nav avec l'id favoriteTeenAgeAlbum : $('#favoriteTeenAgeAlbum a') */
             $(this).removeClass('active');
             $(this).removeAttr('aria-current');
@@ -17,14 +22,12 @@ jQuery(function ($) {
         /* le $(this) suivant correspond lui au lien cliqué */
         $(this).addClass('active');
         $(this).attr('aria-current', 'page');
-        console.log($(this)[0].dataset.target);
+        /*console.log($(this)[0].dataset.target);*/
         const info = $(this)[0].dataset.target;
-        $('#favoriteTeenAgeAlbum .content > div').each(function(){
+        $(`#${parent} .content > div`).each(function(){
             /* $(this) correspond à toutes les div contenant les infos que l'on parcours */
             $(this).addClass('hidden');
         });
-        $(`#favoriteTeenAgeAlbum .content > div#${info}`).removeClass('hidden');
+        $(`#${parent} .content > div#${info}`).removeClass('hidden');
     });
-    
-
 });
