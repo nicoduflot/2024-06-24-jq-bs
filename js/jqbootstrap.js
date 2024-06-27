d1ab2b24c32c0b5b05290f4abe959352f7800f63
unknown <nduflot@dawan.fr>
2024-06-27 09:01:50 +0200

    jquery bootstrap pt1 : préparation des fichiers correction

--- js/jqbootstrap.js
+++ js/jqbootstrap.js
@@ -1,4 +1,8 @@
 //après le chargement complet du DOM
 jQuery(function ($) {
-    
+    $('a.goBottom').on('click', function(e){
+        e.preventDefault();
+        console.log($(document).height());
+        $('html, body').scrollTop( $(document).height() );
+    });
 });
