03c8214c929fd9259b2e8d6669b0c7ee95a169df
unknown <nduflot@dawan.fr>
2024-06-27 11:18:38 +0200

    jquery bootstrap pt3 : navs & tabs dynamiques

--- js/jqbootstrap.js
+++ js/jqbootstrap.js
@@ -5,4 +5,26 @@ jQuery(function ($) {
         console.log($(document).height());
         $('html, body').scrollTop( $(document).height() );
     });
+
+
+    $('#favoriteTeenAgeAlbum a').on('click', function(event){
+        event.preventDefault();
+        $('#favoriteTeenAgeAlbum a').each(function(){
+            /* le $(this) dans ce each() correspond à chaque lien correspondant au sélecteur CSS (les lien contenu dans la nav avec l'id favoriteTeenAgeAlbum : $('#favoriteTeenAgeAlbum a') */
+            $(this).removeClass('active');
+            $(this).removeAttr('aria-current');
+        });
+        /* le $(this) suivant correspond lui au lien cliqué */
+        $(this).addClass('active');
+        $(this).attr('aria-current', 'page');
+        console.log($(this)[0].dataset.target);
+        const info = $(this)[0].dataset.target;
+        $('#favoriteTeenAgeAlbum .content > div').each(function(){
+            /* $(this) correspond à toutes les div contenant les infos que l'on parcours */
+            $(this).addClass('hidden');
+        });
+        $(`#favoriteTeenAgeAlbum .content > div#${info}`).removeClass('hidden');
+    });
+    
+
 });
