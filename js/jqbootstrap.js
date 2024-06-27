e3fcb6648784844796b6dacd92f4359f08bad654
unknown <nduflot@dawan.fr>
2024-06-27 15:09:25 +0200

    jquery bootstrap pt6 : pagination dynamique

--- js/jqbootstrap.js
+++ js/jqbootstrap.js
@@ -1,6 +1,6 @@
 //après le chargement complet du DOM
 jQuery(function ($) {
-    $('.favoriteContent li a').on('click', function(event){
+    $('.favoriteContent li a').on('click', function (event) {
         /* on reprère le clique sur n'importe quel lien situé dans un élément ayant la classe favoriteContent */
         /* on annule le comportement par défaut d'un clic sur un lien */
         event.preventDefault();
@@ -8,7 +8,7 @@ jQuery(function ($) {
         const parent = $(this).closest('.favoriteContent').attr('id');
         /*console.log(parent);*/
         /* on exploite l'id dans le script pour faitre les remplacement de classe et d'attributs */
-        $(`#${parent} a`).each(function(){
+        $(`#${parent} a`).each(function () {
             /* le $(this) dans ce each() correspond à chaque lien correspondant au sélecteur CSS (les lien contenu dans la nav avec l'id favoriteTeenAgeAlbum : $('#favoriteTeenAgeAlbum a') */
             /*$(this).removeClass('active');
             $(this).removeAttr('aria-current');
@@ -20,7 +20,7 @@ jQuery(function ($) {
         $(this).attr('aria-current', 'page');
         /*console.log($(this)[0].dataset.target);*/
         const info = $(this)[0].dataset.target;
-        $(`#${parent} .content > div`).each(function(){
+        $(`#${parent} .content > div`).each(function () {
             /* $(this) correspond à toutes les div contenant les infos que l'on parcours */
             $(this).addClass('hidden');
         });
@@ -33,25 +33,25 @@ jQuery(function ($) {
     table.reactiveModal tbody tr
     */
 
-    $('table.reactiveModal tbody tr').on('click', function(){
-        const parent  = $(this).closest('.reactiveModal').attr('data-bs-target');
+    $('table.reactiveModal tbody tr').on('click', function () {
+        const parent = $(this).closest('.reactiveModal').attr('data-bs-target');
         $(`button.reactiveModal[data-bs-target="${parent}"]`).trigger('click');
         const modalTitle = $(this).closest('.reactiveModal').attr('data-modal-title');
         $(`${parent} .modal-title`).html(modalTitle);
         let modalSections = [];
         let modalSectionsContent = [];
-        $(`[data-bs-target="${parent}"] thead tr th`).each(function(){
+        $(`[data-bs-target="${parent}"] thead tr th`).each(function () {
             console.log($(this).html());
             modalSections.push($(this).html());
         });
         console.log(modalSections);
         console.log($(this)[0].children);
-        for(info of $(this)[0].children){
+        for (info of $(this)[0].children) {
             modalSectionsContent.push(info.innerHTML);
         }
         console.log(modalSectionsContent);
         $(`${parent} .modal-body`).html('');
-        for(key in modalSections){
+        for (key in modalSections) {
             let sectionTitle = `<h3>${modalSections[key]}</h3>`;
             let sectionContent = `<p>${modalSectionsContent[key]}</p>`;
             $(`${parent} .modal-body`).append(sectionTitle);
@@ -59,5 +59,67 @@ jQuery(function ($) {
 
         }
     });
+
+    /* utiliser la pagination : pagination dynamique */
+    $.ajax(
+        {
+            url: 'https://jsonplaceholder.typicode.com/albums',
+            type: 'GET',
+            dataType: 'json'
+        }
+    )
+        .done(function (reponse) {
+            console.log(reponse);
+            const nbElement = reponse.length;
+            const nbPage = Math.ceil(reponse.length / 7);
+            console.log(`nbElement : ${nbElement}, nbPage : ${nbPage}`);
+            /*
+            
+        <li class="page-item"><a class="page-link" href="#">1</a></li>
+        <li class="page-item"><a class="page-link" href="#">2</a></li>
+        <li class="page-item"><a class="page-link" href="#">3</a></li>
+        
+            */
+            let pagination = `
+            <nav aria-label="Page navigation example" id="pages">
+                <ul class="pagination">
+                    <li class="page-item">
+                        <a class="page-link" href="#" aria-label="Previous">
+                            <span aria-hidden="true">&laquo;</span>
+                        </a>
+                    </li>
+        `;
+            /* boucle des pages */
+            for(let i = 0; i < nbPage; i = i + 1){
+                pagination = pagination + `
+                    <li class="page-item">
+                        <a class="page-link" href="#">${i + 1}</a>
+                    </li>
+                `;
+            }
+            pagination = pagination + `
+                    <li class="page-item">
+                        <a class="page-link" href="#" aria-label="Next">
+                            <span aria-hidden="true">&raquo;</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            `;
+            $('#allAlbumsPages').html(pagination);
+            console.log($('[aria-label="Previous"]').parent().next());
+            console.log($('[aria-label="Next"]').parent().prev());
+            $('[aria-label="Previous"]').addClass('disabled');
+            $('[aria-label="Previous"]').parent().next().addClass('active').attr('aria-current', 'page');
+            $('#pages a').on('click', function(event){
+                event.preventDefault();
+            });
+        })
+        .fail(function (erreur) {
+            console.log(erreur);
+        })
+        .always(function () {
+            console.log('requête terminée');
+        });
 
 });
